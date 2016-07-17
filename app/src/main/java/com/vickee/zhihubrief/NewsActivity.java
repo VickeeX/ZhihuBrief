a184b5233934b27ace103a142f12a204848ccf6f
VickeeX <727951723@qq.com>
2016-07-17 17:02:25 +0800

    NewsActivity for a piece of news -- intent with news' id. Next -- how to realize the HTML's body show.

--- app/src/main/java/com/vickee/zhihubrief/NewsActivity.java
+++ app/src/main/java/com/vickee/zhihubrief/NewsActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class NewsActivity extends AppCompatActivity {
+    private static final String TAG = "NewsActivity";
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,9 @@ public class NewsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        id = this.getIntent().getIntExtra("id",0);
+        Log.i(TAG,"id:"+id);
     }
 
 }
